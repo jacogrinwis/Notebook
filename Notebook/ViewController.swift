d1ac2b63f2184d0d12b14fb6ce39b1e3b31126fc
jacogrinwis <jacogrinwis@gmail.com>
2017-09-25 14:07:29 +0200

    Initial Commit

---
+++ Notebook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notebook
+//
+//  Created by Jaco Grinwis on 25-09-17.
+//  Copyright © 2017 Jaco Grinwis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
